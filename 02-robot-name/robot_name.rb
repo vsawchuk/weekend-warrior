51818492af1de9d5ec9c4f1a1caa9b1ae111e4ab
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-08-18 13:42:59 -0700

    Complete robot_name, all tests passing

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,21 @@
+class Robot
 
+  attr_reader :name
+
+  def initialize
+    give_name
+  end
+
+  def reset
+    original = @name
+    until @name != original
+      give_name
+    end
+  end
+
+  def give_name
+    letters = ("A".."Z").to_a
+    @name = letters.sample + letters.sample + rand(0..9).to_s + rand(0..9).to_s + rand(0..9).to_s
+  end
+
+end # end robot class
