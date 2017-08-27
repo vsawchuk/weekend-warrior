6601a6310c412c88e31fde81ed983d0b683acd1d
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-08-27 10:10:54 -0700

    Change diff from class variable to local variable

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,15 +1,15 @@
 class Hamming
 
   def self.compute(string1, string2)
-    @@diff = 0
+    diff = 0
     if string1.length == string2.length
       string1.length.times do |i|
-        @@diff += 1 if string1.split("")[i] != string2.split("")[i]
+        diff += 1 if string1.split("")[i] != string2.split("")[i]
       end
     else
       raise ArgumentError.new("The strings must be the same length")
     end
-    return @@diff
+    return diff
   end
 
 end # end Hamming class
